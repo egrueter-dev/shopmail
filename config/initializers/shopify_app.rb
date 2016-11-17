ceca6168873454a5c8331aa0b2350879570c4113
egrueter <egrueter@jobcase.com>
2016-11-17 14:35:11 -0500

    generate shopify app

---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,7 @@
+ShopifyApp.configure do |config|
+
+  config.api_key = "03dca850fcad294cf5020cb31e461e5f"
+  config.secret = "e22ed12d9cf63262b508d213bedb7cd5"
+  config.scope = "read_orders, read_products"
+  config.embedded_app = true
+end
