59b8bff6e610f005f06d3ccb408609dcd527b2b5
egrueter <egrueter@jobcase.com>
2017-01-07 18:47:40 -0500

    Finally got the damn navbar working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,12 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require materialize-sprockets
+//= require materialize
 //= require_tree .
+
+$('.button-collapse').sideNav({
+  menuWidth: 300, // Default is 240
+  closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+}
+  );
+$('.collapsible').collapsible();
