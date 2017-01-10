1d54d42745ab6002cd6213dd2537d59833428163
egrueter <egrueter@jobcase.com>
2017-01-10 18:57:28 -0500

    Add initial menu routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  resources :settings, only: [:index]
+  resources :campaigns, only: [:index]
+  resources :account_settings, only: [:index]
+
   root :to => 'home#index'
+
   mount ShopifyApp::Engine, at: '/'
 end
