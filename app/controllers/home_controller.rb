131edba66e7dc45f851818e1f9de233836f3f5dd
egrueter <egrueter@jobcase.com>
2017-01-09 21:22:07 -0500

    Add notes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,20 @@
 class HomeController < ShopifyApp::AuthenticatedController
+
+
+  # Index shows:
+  # Can we hardcode these in the database?
+
+  # Total Amount Recovered Order.sum(:value)
+  # http://api.rubyonrails.org/classes/ActiveRecord/Calculations.html
+
+  # Average Order Size Order.average(:value)??
+  # Repeat Customers -- this might be quite hard..
+  # Campaign CTR
+
+
   def index
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
+
+
   end
 end
