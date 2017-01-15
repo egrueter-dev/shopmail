95db6a477d5d7d0dd3d77d2640756fa97d61cd07
egrueter <egrueter@jobcase.com>
2017-01-15 18:45:21 -0500

    campaigns CRUD operations.

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,4 +1,5 @@
 class CampaignsController < ApplicationController
+  before_action :find_campaign, only: [:edit, :update]
 
   def index
     campaigns = Campaign.where(shop_id: session[:shopify])
@@ -6,13 +7,30 @@ class CampaignsController < ApplicationController
     @campaign = campaigns.first
   end
 
+  def edit; end
+
+  def update
+    @campaign.name = params[:name]
+    @campaign.save
+    redirect_to campaigns_path
+  end
+
   def new
     @campaign = Campaign.new
   end
 
   def create
-    campaign = Campaign.new(name: params[:name], shop_id: session[:shopify])
+    campaign = Campaign.new(
+      name: params[:campaign][:name],
+      shop_id: session[:shopify]
+    )
     campaign.save
     redirect_to campaigns_path
+  end
+
+  private
+
+  def find_campaign
+    @campaign = Campaign.find(params[:id])
   end
 end
